43adcb6fe16d1d2203ad86a4274b6df4879d363a
gibitoleon <gabrielleon9928@gmail.com>
2026-09-06 20:24:04 +0300

    chore: configure Redis connection
    Closes #9

--- backend/application-backend/index.js
+++ backend/application-backend/index.js
@@ -1,24 +1,30 @@
 import 'dotenv/config';
 import express from 'express';
 import sequelize from './database/database.config.js';
+import redisClient from './redis/redis.config.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 async function startServer() {
     try {
+
+        // Test Redis connection
+        await redisClient.connect();
+
+       console.log('Redis connected successfully');
         // Test database connection
         await sequelize.authenticate();
 
         console.log('Database connected successfully');
 
-        // Only start Express after DB connection succeeds
+        // Only start Express after redis and  DB connection succeeds
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
 
     } catch (error) {
-        console.error('Database connection failed:', error);
+        console.error('Server startup failed:', error);
         process.exit(1);
     }
 }
---
+++ backend/application-backend/redis/redis.config.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { createClient } from 'redis';
+const client = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT)
+    }
+});
+client.on('error', (err) => {
+            console.error('Redis Client Error', err);
+            process.exit(1);
+        });
+        
+export default client;
+
+
