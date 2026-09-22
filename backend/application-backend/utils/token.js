7bb79a8f59f794ddb260f8f632ea72684880f2ff
gibitoleon <gabrielleon9928@gmail.com>
2026-09-22 20:27:30 +0300

    feat: implement invitation token management
    Closes #26

---
+++ backend/application-backend/utils/token.js
@@ -0,0 +1,16 @@
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
+const generateToken = (bytes = 32) => {
+    return crypto.randomBytes(bytes).toString("hex");
+};
+
+const hashToken = async (token) => {
+    return await bcrypt.hash(token, 10);
+};
+
+ const verifyToken = async (token, tokenHash) => {
+    return await bcrypt.compare(token, tokenHash);
+};
+
+export { generateToken, hashToken, verifyToken}
